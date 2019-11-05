f2aa4840e4346d7a19eee0a67f7f12ecfd50faae
ChiangYuChi <chiangyuchi@gmail.com>
2019-11-05 12:54:07 +0800

    修改 delete 按鈕

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ var UIcontroller = (function () {
             if (type === "inc") {
                 element = DOMstrings.incomeContainer;
                 html =
-                    '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                    '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"> <button class="item__delete--btn"><i class="fas fa-ban"></i></button></div></div></div>';
             } else if (type === "exp") {
                 element = DOMstrings.expensesContainer;
                 html =
